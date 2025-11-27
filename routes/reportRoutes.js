d63f3d7fcc8091631c30ddb5e13e978d25fc9672
fazeel ahmad <fazeelahmadsstit@gmail.com>
2025-11-27 22:13:55 +0500

    ok

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -7,19 +7,20 @@ import {
   deleteReport,
 } from "../controllers/reportController.js";
 import uploadImage from "../utill/uploadimage.js";
+import verifyToken, { verifyAdmin } from "../middleware/auth.js";
 
 const router = express.Router();
 
 // @desc    Create a new report
-router.post("/createReport",uploadImage("files"), createReport);
+router.post("/createReport",verifyToken,uploadImage("files"), createReport);
 
 // @desc    Get all reports
-router.get("/", getReports);
+router.get("/",verifyToken, getReports);
 
 // @desc    Get a single report by ID
-router.get("/:id", getReportById);
+router.get("/:id",verifyToken,verifyAdmin, getReportById);
 
 // @desc    Delete a report by ID
-router.delete("/deleteReport/:id", deleteReport);
+router.delete("/deleteReport/:id",verifyToken,verifyAdmin, deleteReport);
 
 export default router;
