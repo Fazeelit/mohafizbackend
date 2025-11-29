dc74d9e355654750d43e3640509b5bae942eb1c4
fazeel ahmad <fazeelahmadsstit@gmail.com>
2025-11-29 20:12:02 +0500

    update

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -3,7 +3,7 @@ import NewsItem from "../model/NewsModel.js";
 // Create a new news item
 export const createNews = async (req, res) => {
   try {
-    const { title, category, date, published,image } = req.body;
+    const { title, category,description, date, published,image } = req.body;
     const news = new NewsItem({
       title,
       category,
