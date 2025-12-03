7496f426b992f91fb5cf311878cbad799d7df1ba
fazeel ahmad <fazeelahmadsstit@gmail.com>
2025-12-03 17:10:35 +0500

    ji

--- util/uploadVideo.js
+++ util/uploadVideo.js
@@ -34,7 +34,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
-  limits: { fileSize: 1024 * 1024 * 1024 }, // Max 1GB
+  limits: { fileSize: 1024 * 1024 * 2048 }, // 2GB max
   fileFilter: (_req, file, cb) => {
     const allowed = ["video/mp4", "video/avi", "video/mkv", "video/mov"];
     return allowed.includes(file.mimetype)
